8d3ad0234c9206ad78dabe32af97834183b5d1f0
aslupin <poon_arsene_lupin@hotmail.com>
2021-02-25 17:36:16 +0700

    [+] add : refactoring calculation pkg for running suggestion algorithm

--- utility/calculate.go
+++ utility/calculate.go
@@ -1,22 +1,53 @@
 package utility
 
 const (
-	tonPrice = 350
-	tax = 0.01
-	oilPrice = 8
-	depreciation = 800
-	driver = 400
-	fixedCost = 0.05
+	tonPrice 		= 350.0
+	oilPrice 		= 8.0
+	driver 			= 400.0
+	depreciation 	= 800.0
+	rateFixedCost	= 0.05	// 5% of offer
+	rateTax			= 0.01	// 1% of offer
 )
 
-// GetCostFromDistance function for get dcost from distance.
-func GetCostFromDistance(distance float64) float64 {
-	cost := (distance*oilPrice) + (distance*(oilPrice/2)) + ((driver+depreciation)* 1)
-	return cost + (cost * fixedCost) + (cost * tax)
+// GetEnvironmentCostByDay function for get environment cost.
+func GetEnvironmentCostByDay(offer float64, day int) float64 {
+
+	costTruck	:= float64(depreciation	* day)
+	costDriver	:= float64(driver 		* day)
+	costFixed	:= float64(offer) 		* rateFixedCost 
+	costTax		:= float64(offer) 		* rateTax
+	
+	return costDriver + costTruck + costFixed + costTax
+}
+
+// GetCostDrivingCostByDistance function for get driving cost in one time.
+func GetCostDrivingCostByDistance(distance float64, weight int) float64 {
+	var costOilPrice float64
+	
+	if weight > 0 { 
+		costOilPrice = oilPrice		// TRUCK DRIVE WHEN LOADING.
+	} else {
+		costOilPrice = oilPrice / 2	// TRUCK DRIVE WHEN NO LOAD.
+	}
+
+	return distance * costOilPrice
+}
+
+// GetCostOneJob function for get cost from offer and distance.
+func GetCostOneJob(offer, distance float64) float64 {
+	day := 1
+
+	oilPriceDeparture	:= oilPrice
+	oilPriceReturn		:= oilPrice / 2
+	
+	costDriving		:= distance * (oilPriceDeparture + oilPriceReturn) 
+	costEnvironment := GetEnvironmentCostByDay(offer, day)
+
+	return costDriving + costEnvironment
 }
 
 // GetOfferFromWeight function for get offer from weight.
 func GetOfferFromWeight(weight float64) float64 {
-	autoPrice := weight * 350
+	autoPrice := weight * tonPrice
 	return (autoPrice * 0.1) + autoPrice
 }
