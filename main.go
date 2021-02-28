ab837eb771e24446a191c6307242db5bd4625c8c
aslupin <poon_arsene_lupin@hotmail.com>
2021-02-28 21:37:20 +0700

    [+] add : create func for predicting job that job is minimum cost calculating by prepare-cost, jobing-cost and ending-cost

--- main.go
+++ main.go
@@ -3,29 +3,39 @@ package main
 import (
 	"time"
 	"fmt"
+	"container/heap"
+	"github.com/logpost/poc-suggestion-algorithm/pqueue"
 	"github.com/logpost/poc-suggestion-algorithm/utility"
 	"github.com/logpost/poc-suggestion-algorithm/models"
 	"github.com/logpost/poc-suggestion-algorithm/osrm"
 )
 
+var	osrmClient	osrm.OSRM
+
 func main() {
 	
 	// Create OSRM client.
-	osrmClient	:=	osrm.OSRM{}
+	osrmClient		=	osrm.OSRM{}
 	osrmClient.CreateOSRM("http://localhost:5000/")
 
 	// Mocking data
 	jobsMock		:=	utility.LoadJSON()
 
-	jobMockPicked	:=	jobsMock[0].Job
-	// jobsMock		=	jobsMock[1:len(jobsMock) - 1]
+	// Initial cost for each job
+	for index := range jobsMock {
+		jobsMock[index].Job.Cost = utility.GetDrivingCostByDistance(jobsMock[index].Job.Distance, jobsMock[index].Job.Weight)
+	}
 	
+	jobMockPicked	:=	jobsMock[0].Job
+	jobsMock		=	jobsMock[1:]
+
 	// By pass mock data to actual data
-	jobs 		:=	jobsMock
-	jobPicked	:=	jobMockPicked
+	jobs 			:=	&jobsMock
+	jobPicked		:=	jobMockPicked
 
 	// Initial Priority Queue (In-Mem)
-	var	Queue	[]models.Job
+	var Queue	pqueue.PriorityQueue
+	heap.Init(&Queue)
 
 	// Initial variable for running algorithm
 	sumCost			:=	0.0
@@ -34,33 +44,87 @@ func main() {
 	maxWorkingDays	:=	-1
 	startDay	 	:=	time.Now()
 	endDay			:=	time.Now()
-	
+
 	// Initial data selected by user
-	curentLocation		:=	models.Location{
-		Latitude:	float64(14.7995081),
-		Longitude:	float64(100.6533706),
+	originLocation		:=	models.CreateLocation(float64(14.7995081), float64(100.6533706))
+	curentLocation		:=	originLocation
+
+	// Starting suggestion algorithm
+	item	:=	&pqueue.Item{
+		Job:		&jobPicked,
+		JobIndex:	0,
+	}
+	
+	heap.Push(&Queue, item)
+
+	for Queue.Len() > 0 {
+
+		jobPicked	:=	heap.Pop(&Queue).(*pqueue.Item)
+		(*jobs)[jobPicked.JobIndex].Job.Visited	=	true
+
+		sumCost		+=	jobPicked.Job.Cost
+		sumOffer	+=	jobPicked.Job.OfferPrice
+		endDay		=	jobPicked.Job.DropoffDate
+
+		jobPickedLocation	:=	models.CreateLocation(jobPicked.Job.PickUpLocation.Latitude, jobPicked.Job.PickUpLocation.Longitude)
+		prepareRouting		:=	osrmClient.GetRouteInfo(&curentLocation, &jobPickedLocation)
+
+		if prepareRouting	!=	nil {
+			preparingDistance	:=	prepareRouting.Routes[0].Distance
+			preparingCost		:=	utility.GetDrivingCostByDistance(preparingDistance, 0)
+			sumCost				+=	preparingCost
+		}
+
+		minimumIndex, minimumCost	:=	getJobMinimumCost(&jobPickedLocation, &originLocation, jobs)
+		fmt.Println(minimumIndex, minimumCost)
+
+		curentLocation		=	models.CreateLocation(jobPicked.Job.DropOffLocation.Latitude, jobPicked.Job.DropOffLocation.Latitude)
+
 	}
 
-	jobPickedLocation	:=	models.Location{ 
-		Latitude:	float64(jobPicked.PickUpLocation.Latitude),
-		Longitude:	float64(jobPicked.PickUpLocation.Longitude),
+	fmt.Println(Queue, workingDays, maxWorkingDays, startDay, endDay)
+	fmt.Println(sumCost, sumOffer, endDay)
+}
+
+func getJobMinimumCost(curentLocation *models.Location, originLocation *models.Location, jobs *[]utility.JobExpected) (int, float64) {
+	
+	minimumIndex	:=	0
+	minimumCost		:=	9999999.999
+	minimumPrepare	:=	0.0
+	minimumEnd		:=	0.0
+
+	for index	:=	range *jobs {
+
+		if	!(*jobs)[index].Job.Visited {
+			predictingPickUpLocation	:=	models.CreateLocation((*jobs)[index].Job.PickUpLocation.Latitude, (*jobs)[index].Job.PickUpLocation.Longitude)
+			predictingDropOffLocation	:=	models.CreateLocation((*jobs)[index].Job.DropOffLocation.Latitude, (*jobs)[index].Job.DropOffLocation.Longitude)
+
+			prepareRouting				:=	osrmClient.GetRouteInfo(curentLocation, &predictingPickUpLocation)
+			endingRouting				:=	osrmClient.GetRouteInfo(originLocation, &predictingDropOffLocation)
+
+			if prepareRouting != nil && endingRouting != nil {
+				prepareRoutingDistance	:=	prepareRouting.Routes[0].Distance
+				endingRoutingDistance	:=	endingRouting.Routes[0].Distance
+
+				// fmt.Println(index, prepareRoutingDistance, endingRoutingDistance)
+
+				preparingCost			:=	utility.GetDrivingCostByDistance(prepareRoutingDistance, 0)
+				endingCost				:=	utility.GetDrivingCostByDistance(endingRoutingDistance, 0)
+				
+				sumaryPredictingCost	:=	preparingCost + (*jobs)[index].Job.Cost + endingCost
+
+				if	minimumCost > sumaryPredictingCost {
+					fmt.Println("*** MINIMUM: ", index, sumaryPredictingCost)
+					minimumCost		=	sumaryPredictingCost
+					minimumPrepare	=	preparingCost
+					minimumEnd		=	endingCost
+					minimumIndex	=	index
+				}
+			}
+		} 
 	}
 
-	// Get cost of current location to picked job location
-	routingJobPicked	:=	osrmClient.GetRouteInfo(&curentLocation, &jobPickedLocation)
-
-	if routingJobPicked	!=	nil {
-		
-		preparingDistance	:=	routingJobPicked.Routes[0].Distance
-		preparingCost		:=	utility.GetCostDrivingCostByDistance(preparingDistance, 0)
-		sumCost				+=	preparingCost
-	}
-
-	jobPickedCost		:=	utility.GetCostDrivingCostByDistance(jobPicked.Distance, jobPicked.Weight)
-	sumCost				+=	jobPickedCost
-	sumOffer			+=	jobPicked.OfferPrice
-	endDay				=	jobPicked.DropoffDate
-	jobs[0].Job.Visited	=	true
-
-	fmt.Printf("%+v\n%+v", curentLocation, jobPickedLocation)	
+	fmt.Println(minimumIndex, minimumCost, minimumPrepare, minimumEnd)
+	
+	return	minimumIndex, minimumCost
 }
